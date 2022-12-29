eb23906428396cd52fc83e596ced0de12cd89219
leebo-ram <lbr71004@gmail.com>
2022-12-30 00:19:42 +0900

    JS fix : menu filtering

--- main.js
+++ main.js
@@ -1,8 +1,5 @@
 // menu_v2.html 참고사항 : Menu filtering 작업은 div로 2개로 나뉘어져 있으므로 필터링 코드도 2개로 나뉘게 되었다.
 // Menu filtering1 (첫번째줄)
-const wrapBtncontainer2 = document.querySelector('.menu__categories2');
-
-
 const wrapBtncontainer = document.querySelector('.menu__categories1');
 const menucontainer = document.querySelector('.wrap___list');
 const menus = document.querySelectorAll('.wrap');
@@ -23,7 +20,7 @@ wrapBtncontainer.addEventListener('click', (e) => {
 });
 
 // Menu filtering2 (두번째줄)
-// const wrapBtncontainer2 = document.querySelector('.menu__categories2');
+const wrapBtncontainer2 = document.querySelector('.menu__categories2');
 wrapBtncontainer2.addEventListener('click', (e) => {
   const filter = e.target.dataset.filter || e.target.parentNode.dataset.filter;
   if (filter == null) {
