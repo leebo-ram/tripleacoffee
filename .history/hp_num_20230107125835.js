b55c3e01f843625dbb23fbbd99c8cef3a4fa8077
leebo-ram <lbr71004@gmail.com>
2023-01-07 13:39:09 +0900

    JS: stampMemberCheck.html - 전화번호 버튼 클릭時 자동 하이픈 삽입 기능 추가 (add Button click auto hypen)

---
+++ .history/hp_num_20230107125835.js
@@ -0,0 +1,53 @@
+'use strict';
+
+// stampMemberCheck.html - 핸드폰 번호 입력
+window.addEventListener('click', (e) => {
+  console.log(e.target)
+  const target = document.getElementById('mobileNumber');
+
+
+  // Button 클릭시 target에 입력
+  if(e.target.closest('button').dataset.val) {
+    if(target.value.length < 11) {
+      console.log(target);
+      target.value += e.target.closest('button').dataset.val;
+      console.log(target.value.length) 
+    }
+  }
+
+  // 전체 삭제
+  if(e.target.closest('button').className == 'number__del__ALL' ) {
+    target.value = "";
+  }
+
+  // 끝에서 하나만 삭제
+  if(e.target.closest('button').className == 'number__pop') {
+    target.value = target.value.slice(0, -1)
+  }
+
+  // 전화번호 하이픈 자동생성
+  target.value = target.value
+  .replace(/[^0-13]/g, '')
+  .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+
+
+
+});
+
+
+  // 입력 길이제한
+function handleOnInput(el, maxlength) {
+  if(el.value.length > maxlength)  {
+    el.value 
+      = el.value.substr(0, maxlength);
+  }
+}
+
+
+
+// 전화번호 하이픈 자동생성 정규식
+// const autoHyphen2 = (target) => {
+//   target.value = target.value
+//     .replace(/[^0-9]/g, '')
+//   .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3").replace(/(\-{1,2})$/g, "");
+// }
